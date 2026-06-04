bb23d79a056293a175e5fd640680019a0fb004a1
Shankyyi <https://github.com/Shankyyi>
2026-06-05 00:10:34 +0530

    Day1 uplodaded.

---
+++ DAY1/Program4.py
@@ -0,0 +1,15 @@
+num = int(input("Enter a number: "))
+
+# Convert negative number to positive
+num = abs(num)
+
+count = 0
+
+if num == 0:
+    count = 1
+else:
+    while num > 0:
+        num = num // 10
+        count += 1
+
+print("Number of digits:", count)
